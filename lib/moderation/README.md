78f8b94fe90abe2b45ac33d699cfd9e603686a7e
intrnl <intrnl28+git@gmail.com>
2023-09-06 10:27:05 +0700

    refactor!: remove moderation code
    this isn't even something that i use for myself, so removing it

--- lib/moderation/README.md
+++
@@ -1,3 +0,0 @@
-https://github.com/bluesky-social/atproto/pull/1366
-
-https://github.com/bluesky-social/atproto/tree/2863240e0607e9c0170d4b2549f07bbdc837d707/packages/api/src/moderation
--- lib/moderation/index.ts
+++
@@ -1,6 +0,0 @@
-export { LABEL_GROUPS } from './constants/label-groups.js';
-export { LABELS } from './constants/labels.js';
-
-export * from './moderate.js';
-
-export type * from './types.js';
